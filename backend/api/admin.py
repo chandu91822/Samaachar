7cd459a363306a681883e4b3260100233b0eea7c
chandu91822 <chandukadiri96420@gmail.com>
2025-11-14 16:56:08 +0530

    tables_updated

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (
+    Plan, Subscription, Address, DeliveryAssignment,
+    DeliveryBoyStats, Bill, Payment, Complaint,
+    SubscribeRequest, ChangeRequest, PauseRequest
+)
+
+# ------------------ SIMPLE REGISTRATION ------------------
+
+admin.site.register(Plan)
+admin.site.register(Subscription)
+admin.site.register(Address)
+admin.site.register(DeliveryAssignment)
+admin.site.register(DeliveryBoyStats)
+admin.site.register(Bill)
+admin.site.register(Payment)
+admin.site.register(Complaint)
+admin.site.register(SubscribeRequest)
+admin.site.register(ChangeRequest)
+admin.site.register(PauseRequest)
+
 
 
